59c5889de97ab28d427d68b0d5e6b9cfce6cc4f0
Marcin <marcin92d@gmail.com>
2023-01-19 14:17:39 +0100

    beer-service | @ComponentScan add to BeerControllerTest

--- beer-service/src/test/java/pl/dolega/beerservice/web/controller/BeerControllerTest.java
+++ beer-service/src/test/java/pl/dolega/beerservice/web/controller/BeerControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,6 +17,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "pl.dolega.beerservice.web.mapper")
 class BeerControllerTest {
 
     @Autowired
