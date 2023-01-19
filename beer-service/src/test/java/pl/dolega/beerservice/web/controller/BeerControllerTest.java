24a206a0e9baa6df7928d317ddb9f41630c7d39a
Marcin <marcin92d@gmail.com>
2023-01-19 10:47:38 +0100

    changing jdk to 11

--- beer-service/src/test/java/pl/dolega/beerservice/web/controller/BeerControllerTest.java
+++ beer-service/src/test/java/pl/dolega/beerservice/web/controller/BeerControllerTest.java
@@ -1,6 +1,7 @@
 package pl.dolega.beerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.dolega.beerservice.web.model.BeerDto;
+import pl.dolega.beerservice.web.model.BeerStyle;
 
 import java.util.UUID;
 
@@ -22,6 +24,17 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    BeerDto validBeer;
+
+    @BeforeEach
+    public void setUp() {
+        validBeer = BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Kido")
+                .beerStyle(BeerStyle.ALE)
+                .upc(123456789012L)
+                .build();
+    }
+
     @Test
     void getBeerById() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
@@ -31,7 +44,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = validBeer;
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer")
@@ -42,7 +55,7 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = validBeer;
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
